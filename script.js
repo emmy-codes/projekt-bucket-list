33dc5b500e4ce86ba795304f6dc2115f89cfccab
Emma <alunia.moon@gmail.com>
2025-01-22 13:22:15 +0100

    bucket list commit for adding and appending items to DOM - step 1

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 // array to hold list items
+const bucketListItems = [];
 
-let bucketListItems = [];
+// get reference of input field and select option
+const inputData = document.querySelector("#activityName");
+const selectData = document.querySelector("#activityCategory");
 
-const displayBucketList = () => {
-    // let targetList = document.querySelector("bucketLists");
-    // let ul = document.createElement("ul")
-    // let newListItem = document.createElement("li");
-    // ul.appendChild(newListItem).innerHTML = "Practice time";
-    // console.log(ul.childNodes)
+const displayBucketList = (event) => {
+    // create actual list but not appended, gives it an id
+    const list = document.createElement("ul");
+    list.id = "ul-id";
 
-    const targetList = document.querySelector("#bucketLists");
-    targetList.innerHTML += `<ul><li>Test test</li></ul>`
+    // collecting value from the input/select and clearing form on submit
+    event.preventDefault();
+    const inputValue = inputData.value;
+    const selectValue = selectData.value;
+    inputData.value = "";
+    selectData.value = "";
+    console.log(inputValue, selectValue);
+
+    // add items to bucketListItems array
+    bucketListItems.push({
+        name: inputValue,
+        category: selectValue
+    });
+
+    // maps array items to LI elements and append them to the list
+    bucketListItems.map((bucketListItem) => {
+        const newItem = document.createElement("li");
+        newItem.innerHTML = `${bucketListItem.name} - ${bucketListItem.category}`
+        list.appendChild(newItem);
+    })
+
+    // grab reference for bucket lists
+    const bucketLists = document.querySelector("#bucketLists");
+
+    // grab possible reference to a possible reference to the list that may exist on the DOM
+    const currentList = document.querySelector("#ul-id");
+
+    // check if the list exists, if so, remove it to avoid duplicates
+    if (currentList) {
+        bucketLists.removeChild(currentList)
+    }
+
+    // append the new list (with all and any added items) to the DOM
+    bucketLists.appendChild(list);
+    
 };
 
-displayBucketList();
+// event listener 
+const submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("click", displayBucketList);
