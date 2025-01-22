a05465d024f44eb48bc1492dfbcfc675e1cf92a0
Emma <alunia.moon@gmail.com>
2025-01-23 00:20:19 +0100

    refactoring position of code snippets for better flow and correctly remove items from the DOM on delete

--- script.js
+++ script.js
@@ -5,11 +5,36 @@ const bucketListItems = [];
 const inputData = document.querySelector("#activityName");
 const selectData = document.querySelector("#activityCategory");
 
+// initiate list as null
+let currentList = null;
+
+// grab reference for bucket lists
+const bucketLists = document.querySelector("#bucketLists");
+
+// delete list item
+const deleteItem = (indexPosition) => {
+    alert("Are you sure you'd like to delete this?");
+    bucketListItems.splice(indexPosition, 1);
+};
+
+const updatedList = (list) => {
+    // check if the list exists, if so, remove it to avoid duplicates
+    if (currentList) {
+        bucketLists.removeChild(currentList);
+    }
+
+    // append the new list (with all and any added items) to the DOM
+    bucketLists.appendChild(list);
+}
+
 const displayBucketList = (event) => {
     // create actual list but not appended, gives it an id
     const list = document.createElement("ul");
     list.id = "ul-id";
-    
+
+    // grabs the current reference of the list from the DOM
+    currentList = document.querySelector("#ul-id");
+
     // collecting value from the input/select and clearing form on submit
     event.preventDefault();
     const inputValue = inputData.value;
@@ -20,51 +45,38 @@ const displayBucketList = (event) => {
     // add items to bucketListItems array
     bucketListItems.push({
         name: inputValue,
-        category: selectValue
+        category: selectValue,
     });
 
     // maps array items to LI elements and append them to the list
     bucketListItems.map((bucketListItem, indexPosition) => {
         const newItem = document.createElement("li");
-        newItem.innerHTML = `${bucketListItem.name} - ${bucketListItem.category}`
+        newItem.innerHTML = `${bucketListItem.name} - ${bucketListItem.category}`;
         // adding delete button to LIs
         const deleteButton = document.createElement("button");
-        deleteButton.id = "delete-btn"
+        deleteButton.id = "delete-btn";
         deleteButton.textContent = "Delete";
+
+        newItem.appendChild(deleteButton);
+
+        // appending newItem and deleteButton to the list
         list.appendChild(newItem);
-        list.appendChild(deleteButton);
+        deleteButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            // remove the item from the array (bucketListItems)
+            deleteItem(indexPosition);
+
+            // remove the item from the list (DOM)
+            list.removeChild(newItem);
+
+            // update the list
+            updatedList(list);
+        });
     });
 
-    // grab reference for bucket lists
-    const bucketLists = document.querySelector("#bucketLists");
-
-    // grab possible reference to a possible reference to the list that may exist on the DOM
-    const currentList = document.querySelector("#ul-id");
-
-    // check if the list exists, if so, remove it to avoid duplicates
-    if (currentList) {
-        bucketLists.removeChild(currentList)
-    }
-
-    // append the new list (with all and any added items) to the DOM
-    bucketLists.appendChild(list);
-    const deleteItem = () => {
-        // alert("Are you sure you'd like to delete this?");
-        // console.log("definitely deleted yep");
-        for (i = 0; i < bucketListItems.length; i++) {
-            if (bucketListItems[i]) {
-                console.log("nailed it")
-            }
-        }
-    };
-    // delete item
-    const deleteItemButton = document.querySelector("#delete-btn");
-    deleteItemButton.addEventListener("click", deleteItem)
+    updatedList(list);
 };
-
-
 
 // event listeners
 const submitButton = document.querySelector("#submit-button");
 submitButton.addEventListener("click", displayBucketList);
-
